e12cdabe5663a2063e172b6987f2a8eef072cd98
tamjidahmed <mail.to.tamjid@gmail.com>
2021-09-12 21:28:03 +0600

    Full cycle!!

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "log"
+import (
+	"alien/simulator"
+	"alien/world"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	log.Println("Ok")
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fileName := fmt.Sprintf("%s/data2.txt", dir)
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() { _ = file.Close() }()
+
+	var numAlien int
+	if len(os.Args) >= 2 {
+		numAlien, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	w := world.CreateFromFile(file)
+	conn := w.GetConnections()
+	sim, err := simulator.NewSimulator(conn, numAlien, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = sim.Simulate()
+	if !errors.Is(err, simulator.ErrMaxIterationReached) && !errors.Is(err, simulator.ErrNoAlienAlive) {
+		log.Fatalln(err)
+	}
+	fmt.Println("Report: ------------------------")
+	w.Report(sim.GetDestroyedCities())
 }
---
+++ world/world.go
@@ -0,0 +1,97 @@
+package world
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type World struct {
+	connections map[string][]city
+	inputOrder  []string
+}
+
+type city struct {
+	name        string
+	orientation string
+}
+
+func CreateFromFile(f io.ReadCloser) *World {
+	defer func() { _ = f.Close() }()
+	world := &World{
+		connections: make(map[string][]city),
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		curCity := ""
+		for i, part := range parts {
+			// i == 0 means the current city name
+			if i == 0 {
+				world.inputOrder = append(world.inputOrder, part)
+				world.connections[part] = make([]city, 0)
+				curCity = part
+				continue
+			}
+			orientation, neighbour := func(s string) (string, string) {
+				x := strings.Split(part, "=")
+				return x[0], x[1]
+			}(part)
+			world.connections[curCity] = append(world.connections[curCity],
+				city{name: neighbour, orientation: orientation})
+		}
+	}
+	return world
+}
+
+func (w *World) GetConnections() map[string][]string {
+	conn := make(map[string][]string)
+	for cityName, neighbours := range w.connections {
+		if _, ok := conn[cityName]; !ok {
+			conn[cityName] = make([]string, 0)
+		}
+		for _, neighbour := range neighbours {
+			neighbourName := neighbour.name
+			conn[cityName] = append(conn[cityName], neighbourName)
+			// Connections are bidirectional
+			if _, ok := conn[neighbourName]; !ok {
+				conn[neighbourName] = make([]string, 0)
+			}
+			conn[neighbourName] = append(conn[neighbourName], cityName)
+		}
+	}
+	// De-duplicate neighbours
+	for cityName, neighbours := range conn {
+		uniqueNeighbours := make(map[string]bool)
+		for _, name := range neighbours {
+			uniqueNeighbours[name] = true
+		}
+		list := make([]string, 0)
+		for name := range uniqueNeighbours {
+			list = append(list, name)
+		}
+		conn[cityName] = list
+	}
+	return conn
+}
+
+func (w *World) Report(destroyedCities map[string]bool) {
+	for _, curName := range w.inputOrder {
+		if destroyedCities[curName] {
+			continue
+		}
+		neighbours := w.connections[curName]
+		var sb strings.Builder
+		for _, city := range neighbours {
+			if destroyedCities[city.name] {
+				continue
+			}
+			sb.WriteString(fmt.Sprintf(" %s=%s", city.orientation, city.name))
+		}
+		if sb.Len() == 0 {
+			sb.WriteString(" ---")
+		}
+		fmt.Println(curName, sb.String())
+	}
+}
